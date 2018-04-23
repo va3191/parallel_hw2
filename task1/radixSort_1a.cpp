37fcb9fce90ea70df41d92f7d9c82cdc3b8ff62f
Varun Agarwal <varun31@login2.stampede2.tacc.utexas.edu>
2018-04-22 21:11:19 -0500

    adding task 1

---
+++ task1/radixSort_1a.cpp
@@ -0,0 +1,186 @@
+#include <math.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include<time.h>
+#include<iostream>
+#include <cmath>
+#include <vector>
+#include<chrono>
+#include<array>
+#include <ctime>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <cilk/cilk.h>
+#include <cilk/cilk_api.h>
+using namespace std::chrono;
+uint64_t g_seed = time(0);
+
+
+void printArray(std::vector<int> &A, int size){
+
+    for (int i = 0; i < size ; i++) {
+        printf("%   d", A[i]);
+        if ( i != size-1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+void parallel_prefix_sum(std::vector<int> &arr, int n, std::vector<int> &index) {
+  
+   if (n == 1) {
+       index[0] = arr[0];
+       return;
+   }
+
+   std::vector<int> arr1(n/2, 0);
+   std::vector<int> arr2(n/2, 0);
+
+   for(int i = 0; i < n/2; ++i) {
+      arr1[i] = arr[2 * i] + arr[(2 * i) + 1];
+   }
+
+   parallel_prefix_sum(arr1, n/2, arr2);
+
+   for (int i = 0; i < n; ++i) {
+      if (i == 0) {
+          index[0] = arr[0];
+      } else if (i % 2 == 1) {
+          index[i] = arr2[i / 2];
+      } else {
+          index[i] = arr2[(i - 1)/2] + arr[i];
+      }
+   }
+
+}
+
+void par_Counting_Rank (std::vector<int> &array, int n, int d, std::vector<int> &r, int p  ){
+
+    std::vector< std::vector <int> > f( pow(2,d), std::vector<int>(p));
+    std::vector< std::vector <int> > r1( pow(2,d), std::vector<int>(p));
+    std::vector<int> js(p);
+    std::vector<int> je(p);
+    std::vector<int> ofs(p);
+
+
+    cilk_for (int i =0;i < p; i++){
+        for (int j =0;j< pow(2,d);j++){
+            f[j][i]=0;
+        }
+        js[i]= (i)*floor(n/p);
+
+        if(i<p-1){
+            je[i]=((i+1) * floor(n/p) )-1;
+        }else{
+           je[i]=n-1;
+        }
+        for (int j =js[i];j<=je[i];j++){
+            f[array[j]][i] +=  1;
+        }
+
+    }
+
+    for (int j =0;j< pow(2,d);j++){
+            std::vector<int> temp(p, 0);
+            parallel_prefix_sum(f[j], p, temp); 
+            f[j]=temp;
+
+        }
+
+    cilk_for (int i =0;i< p;i++){
+        ofs[i]=0;
+        for (int j =0;j< pow(2,d);j++){
+            if(i==0){
+                r1[j][i] = ofs[i];
+            }else{
+                r1[j][i] =ofs[i] + f[j][i-1];
+            }
+            ofs[i]=ofs[i]+f[j][p-1];
+        }
+        for (int j =js[i];j<=je[i];j++){
+            r[j] = r1[array[j]][i];
+            r1[array[j]][i] = r1[array[j]][i] + 1;
+        }       
+    }
+
+}
+
+int ExtractBitSegment(int n, int start, int q) {
+    int end = start +q-1;
+    unsigned int mask = ~(~0 << (end - start + 1));
+    return mask & (n >> start);
+}
+void par_radix_sort_with_counting(std::vector<int> &arr, int n , int bits, int p) {
+    std::vector<int> S(n);
+    std::vector<int> r(n);
+    std::vector<int> b(n);
+    int q;
+    float l = log( (float(n) )/ ( p*log(n) ) );
+    int d =ceil(l);
+
+    for (int k =0;k< bits;k++){
+
+        if(k + d <= bits){
+             q = d;
+        }else{
+             q  = bits -k ;
+        }
+        cilk_for(int i =0;i<n;i++){
+            S[i] = ExtractBitSegment(arr[i], k,q);    
+        }
+        par_Counting_Rank(S, n, q,r,p);
+
+        cilk_for(int i =0;i<n;i++){
+            b[r[i]]=arr[i];
+        }
+        cilk_for(int i =0;i<n;i++){
+            arr[i]=b[i];
+        }
+    }
+
+}
+
+void checkIfSorted(std::vector<int> &arr , int size){
+    int flag=1;
+    for(int i =0; i<size;i++){
+        if(arr[i]>arr[i+1]){
+           printf("array is not sorted %d ..... %d,.....%d\n ", arr[i],arr[i+1],i); 
+
+           flag=0;
+        }
+    }
+    if(flag){
+        printf("Array is sorted\n");
+    }
+}
+int randomCoinToss() { 
+        g_seed = (214013*g_seed+2531011);
+        return ((g_seed>>16)&0x7FFF)%1024;
+    }
+int main(int argc, char **argv) {
+
+    double start, stop, time_elapsed;
+     __cilkrts_set_param("nworkers","10");
+    int arraySize = atoi(argv[1]);
+    std::vector<int> arr(arraySize, 0);
+    for(int i = 0; i < arraySize; ++i) {
+       arr[i] = randomCoinToss() % 1024;
+   }
+    printf("Unsorted Array\n"); 
+    std::cout<<"calling   "<<arraySize<<std::endl;
+
+    __int64 now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    par_radix_sort_with_counting(arr, arraySize,10,10);
+    __int64 now1 = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    printf("Sorted Array\n");
+    checkIfSorted(arr, arraySize-1);
+    float timeTaken=float (now1- now);
+
+
+    std::cout<<"time taken  ::"<<timeTaken<<std::endl;
+        
+    return 0;
+}
---
+++ task1/varun_quickSort_varyingCore_1c.cpp
@@ -0,0 +1,207 @@
+#include <cilk/cilk.h>
+#include <cilk/cilk_api.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include<time.h>
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <limits.h>
+#include <time.h>
+#include <math.h>
+#include<chrono>
+
+int *lt;
+int *gt;
+long *copyArray;    
+using namespace std::chrono;
+int m;
+int random_int (unsigned int low, unsigned int high)
+{
+    int random = rand();
+    if (RAND_MAX == random) {
+        return random_int(low, high);
+    }
+
+    int range = high - low,
+        remain = RAND_MAX % range,
+        slot = RAND_MAX / range;
+
+    if (random < RAND_MAX - remain) {
+        return low + random / slot;
+    } else {
+        return random_int (low, high);
+    }
+}
+/* printf if PRINTMODE is defined */
+
+void printArray(long *A, int lo, int hi){
+    int i;
+
+    printf("[");
+    for (i = lo; i <= hi ; i++) {
+        printf("%ld", A[i]);
+        if ( i != hi) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+}
+
+/* print an array if PRINTMODE is defined */
+
+
+void parallel_prefix_sum(int *lt, int *gt,  int start, int n, int k) {
+
+    int i, h;
+    for(h = 1 ; h <= k ; h++) {
+        cilk_for (i = 1; i <= (n >> h); i++) {
+            lt[start+i * (1 << h) - 1] += lt[start+(1 << h) * i - (1 << (h-1)) - 1];
+            gt[start+i * (1 << h) - 1] +=  gt[start+(1 << h) * i - (1 << (h-1)) - 1];
+        }
+    }
+
+    for (h = k ; h >= 1; h--) {
+        cilk_for(i = 2; i <= (n >> (h-1)); i++){
+            if (i % 2) {
+                lt[start+i * (1 << (h-1)) -1] += lt[start+i * (1 << (h-1)) - (1 << (h-1)) - 1];
+                gt[start+i * (1 << (h-1)) -1] += gt[start+i * (1 << (h-1)) - (1 << (h-1)) - 1];
+            }
+        }
+    }
+
+}
+
+void sequential_insertionSort(long *array, int left, int right) {
+
+    int i, j, val;
+    for(i = left; i <= right; i++) {
+        val = array[i];
+        j = i - 1;
+        while( j >= 0 && array[j] > val) {
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = val;
+    }
+}
+
+int partition(long *array, int left, int right){
+
+    int n = (right - left + 1),
+        k = (int) log2(n),
+        i;
+
+    long pivot = array[right];
+
+    cilk_for (i = left; i <= right; i++) {
+
+        copyArray[i] = array[i];
+
+        if (array[i] < pivot) {
+            lt[i]=1;
+            gt[i]=0;
+        }
+        else if(array[i]>pivot) {
+            lt[i]=0;
+            gt[i]=1;
+        }
+        else{
+            if(i==right){
+                lt[i]=0;
+            }
+            else {
+                lt[i]=1;
+            }
+            gt[i]=0;
+        }
+    }
+
+    parallel_prefix_sum(lt, gt, left, n,k);
+
+    int pivotIndex = left+lt[right];
+    array[pivotIndex] = pivot;
+
+    cilk_for (i = left; i < right; i++){
+        if(copyArray[i]<=pivot){
+            array[left+lt[i]-1] = copyArray[i];
+        }
+        else if (copyArray[i]>pivot){
+            array[pivotIndex+gt[i]] = copyArray[i];
+        }
+    }
+
+    return pivotIndex;
+}
+
+void quicksort(long *array,int left,int right){
+
+    if(left >= right) {
+        return;
+    }
+
+    if (right - left + 1 <= m) {
+        return sequential_insertionSort(array, left, right);
+    }
+    else {
+        int splitPoint;
+
+            splitPoint = partition(array,left, right);
+
+        cilk_spawn quicksort(array,left,splitPoint-1);
+        quicksort(array,splitPoint+1,right);
+    }
+}
+
+void checkIfSorted(long *arr, int size){
+    int flag=1;
+    for(int i =0; i<size;i++){
+        if(arr[i]>arr[i+1]){
+           printf("array is not sorted %ld ..... %ld\n ", arr[i],arr[i+1]); 
+           flag=0;
+        }
+    }
+    if(flag){
+        printf("Array is sorted\n");
+    }
+}
+uint64_t g_seed = time(0);
+static int randomCoinToss() { 
+        g_seed = (214013*g_seed+2531011);
+        return ((g_seed>>16)&0x7FFF)%1024;
+    }
+int main(int argc, char **argv) {
+
+    double start, stop, time_elapsed;
+    m =1024;
+     __cilkrts_set_param("nworkers", argv[1]);
+   int length = pow(2,20);
+    copyArray = (long *) malloc (sizeof(long) * length);
+    lt = (int *) malloc (sizeof(int) * length);
+    gt = (int *) malloc (sizeof(int) * length);
+    for(int i = 0 ; i < length ; i++) {
+        copyArray[i] = 0;
+        lt[i] = 0;
+        gt[i] = 0;
+    }
+
+    long* array = (long *) malloc(length * sizeof(long));
+    int i;
+   for(i = 0; i < length; i++){
+        array[i] =randomCoinToss();
+    }
+    printf("Unsorted Array\n");
+   /* printArray(array,0, length-1);*/
+    __int64 now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    quicksort(array, 0,length-1);
+    __int64 now1 = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    printf("Sorted Array\n");
+    /*printArray(array,length);*/
+    checkIfSorted(array , length-1); 
+    float timeTaken=float(now1- now);
+    std::cout<<timeTaken<<std::endl;
+    return 0;
+}
